16f2ab3b4cb93b9849ccb6fa18946cbfe114be55
Joe Gesualdo <joegesualdo@gmail.com>
2015-10-08 13:18:34 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Phrases 
+> List of english phrases 
+
+## Install
+```
+$ npm install --save phrases 
+```
+
+## Usage
+```javascript
+var Phrases = require('phrases')
+
+Phrases.getPhrases(function(phrases){
+  console.log(phrases)
+})
+```
+
+## API
+### `getPhrases(callback)`
+
+```javascript
+var Phrases = require('phrases')
+
+Phrases.getPhrases(function(phrases){
+  console.log(phrases)
+})
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs')
+var path = require('path')
+var readline = require('readline')
+
+var Phrases = {
+  getPhrases: getPhrases
+}
+
+function getPhrases(callback){
+  phrases= []
+  getPhrasesDotOrgPhrases(function(phrasesDotOrgPhrases){
+    phrases = phrases.concat(phrasesDotOrgPhrases)
+    callback(phrases)
+  })
+}
+function getPhrasesDotOrgPhrases(callback){
+  var phrasesDotOrgPhrasesList = []
+  var rl = readline.createInterface({
+    input: fs.createReadStream(__dirname + '/phrases_dot_org.txt')
+  });
+
+  rl.on('line', function(line) {
+    phrasesDotOrgPhrasesList.push(line)
+  });
+  rl.on('close', function(){
+    callback(phrasesDotOrgPhrasesList)
+  })
+}
+
+module.exports = Phrases 
+
+Phrases.getPhrases(function(results){
+  console.log(results)
+})
